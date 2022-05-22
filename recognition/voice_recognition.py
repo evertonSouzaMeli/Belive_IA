37e6480db2b3943b44aac8d2a53a8d8b22e7dede
evertonSouzaMeli <everton.vsilva@mercadolivre.com>
2022-05-22 18:21:33 -0300

    feat: image recognition complete

--- recognition/voice_recognition.py
+++ recognition/voice_recognition.py
@@ -11,59 +11,54 @@ from speech_recognition import WaitTimeoutError, UnknownValueError
 def voiceCommands():
     welcome()
 
-    print("Para começar diga ok")
-    speak("Para começar diga ok")
-    resp = microphone().lower()
-
     while True:
         try:
-            if resp == 'ok':
-                print("Sim. O que devo fazer?")
-                speak("Sim. O que devo fazer?")
+            print("Sim. O que devo fazer?")
+            speak("Sim. O que devo fazer?")
 
-                desire = microphone().lower()
-                print(desire)
+            desire = microphone().lower()
+            print(desire)
 
-                if desire == 'criar evento':
-                    print('Qual evento devo cadastrar?')
-                    speak('Qual evento devo cadastrar?')
+            if desire == 'criar evento':
+                print('Qual evento devo cadastrar?')
+                speak('Qual evento devo cadastrar?')
 
-                    save_event(microphone())
-                    print('Evento criado com sucesso')
-                    speak('Evento criado com sucesso')
+                save_event(microphone())
+                print('Evento criado com sucesso')
+                speak('Evento criado com sucesso')
 
-                elif desire == 'ler agenda':
-                    events = read_event().split(', ')
+            elif desire == 'ler agenda':
+                events = read_event().split(', ')
 
-                    for event in events:
-                        print(event)
-                        speak(event)
+                for event in events:
+                    print(event)
+                    speak(event)
 
-                elif desire == 'fale data e hora':
-                    print(date())
-                    speak(date())
+            elif desire == 'fale data e hora':
+                print(date())
+                speak(date())
 
-                elif desire == 'toque uma música':
-                    print("tocando music")
-                    music()
+            elif desire == 'toque uma música':
+                print("tocando music")
+                music()
 
-                elif desire == 'me diga como está o tempo hoje':
-                    print(temp())
-                    speak(temp())
+            elif desire == 'me diga como está o tempo hoje':
+                print(temp())
+                speak(temp())
 
-                elif desire == 'quero escutar uma música':
-                    webbrowser.open("https://www.youtube.com/watch?v=f2D2hEFnHLU")
-                    print("Vou te mostrar o que é musica de verdade, essa é do balácu bácu")
-                    speak("Vou te mostrar o que é musica de verdade, essa é do balácu bácu")
+            elif desire == 'quero escutar uma música':
+                webbrowser.open("https://www.youtube.com/watch?v=f2D2hEFnHLU")
+                print("Vou te mostrar o que é musica de verdade, essa é do balácu bácu")
+                speak("Vou te mostrar o que é musica de verdade, essa é do balácu bácu")
 
-                elif desire == 'abrir youtube':
-                    webbrowser.open("youtube.com")
-                    print("Youtube aberto.")
-                    speak("Youtube aberto.")
+            elif desire == 'abrir youtube':
+                webbrowser.open("youtube.com")
+                print("Youtube aberto.")
+                speak("Youtube aberto.")
 
-                else:
-                    print('Desculpa, não entendi o que você está querendo dizer. Poderia repetir?')
-                    speak('Desculpa, não entendi o que você está querendo dizer. Poderia repetir?')
+            else:
+                print('Desculpa, não entendi o que você está querendo dizer. Poderia repetir?')
+                speak('Desculpa, não entendi o que você está querendo dizer. Poderia repetir?')
 
         except WaitTimeoutError:
             pass
